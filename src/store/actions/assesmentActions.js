0ad3eb463109ecbeea27b8668fdb29602484b157
Hammad Zafar <hammadzafar13@gmail.com>
2024-03-24 20:17:43 +0500

    attemp exam and oga submit implememted

--- src/store/actions/assesmentActions.js
+++ src/store/actions/assesmentActions.js
@@ -16,3 +16,27 @@ export const getAssesments = asyncCatch(
         handleError(error);
       })
 );
+
+export const getExamById = asyncCatch(
+  "assesmentsSlice/getExamById",
+  async ({ config, query }) =>
+    axios
+      .get(
+        `${CONSTANTS.VITE_BACKEND_API_URL}/api/exams/${query.examId}`,
+        config
+      )
+      .then((resp) => resp)
+      .catch((error) => {
+        handleError(error);
+      })
+);
+
+export const submitExamByStudent = asyncCatch(
+  "quizSlice/submitExamByStudent",
+  async ({ config, body }) =>
+    axios.post(
+      `${CONSTANTS.VITE_BACKEND_API_URL}/api/exams/submit`,
+      body,
+      config
+    )
+);
