fefbc9626cf00b2bf7f6f3f39d7227f30612d893
Hammad Zafar <hammadzafar13@gmail.com>
2024-05-02 02:54:02 +0500

    Manage students and manage teachers lisitng page done

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -2,10 +2,14 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
+  const userType = localStorage.getItem("userType");
   const { loginUserData } = useSelector((s) => s.authReducer);
-  return <Outlet />;
 
-  // return loginUserData?.token ? <Outlet /> : <Navigate to="/login" replace />;
+  if (userType === "admin") {
+    return <Outlet />;
+  }
+
+  return loginUserData?.token ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
